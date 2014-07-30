4f4a33c63c607ee0d340711c7e67ba23e217a591
dantezhu <dantezhu@qq.com>
2014-07-30 18:49:41 +0800

    版本升级

--- melon/__init__.py
+++ melon/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.27'
+__version__ = '1.0.28'
 
 from .melon import Melon
 from .blueprint import Blueprint
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="melon",
-    version='1.0.27',
+    version='1.0.28',
     zip_safe=False,
     platforms='any',
     packages=['melon'],
