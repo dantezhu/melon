1b11dca4166a6cb5ab7a84445aec74c648eaf331
dantezhu <dantezhu@qq.com>
2014-08-25 15:33:42 +0800

    删掉没用的

--- examples/benchmark_test/reimp.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from netkit.box import Box as Box
-
-import logging
-from melon import logger, Melon, Blueprint
-
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.DEBUG)
