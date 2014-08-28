e3e43491fd65b942fec2a2eb3a81d2038aaaa702
dantezhu <dantezhu@qq.com>
2014-08-28 14:41:22 +0800

    _reg_event_handler

--- melon/mixins.py
+++ melon/mixins.py
@@ -40,10 +40,10 @@ class RoutesMixin(object):
         return self.rule_map.get(cmd)
 
 
-def _register_handler(func):
+def _reg_event_handler(func):
     @functools.wraps(func)
-    def func_wrapper(mixin, handler):
-        event = getattr(mixin.events, func.__name__)
+    def func_wrapper(obj, handler):
+        event = getattr(obj.events, func.__name__)
         event += safe_func(handler)
 
         return handler
@@ -56,42 +56,42 @@ class AppEventsMixin(object):
     def __init__(self):
         self.events = Events()
 
-    @_register_handler
+    @_reg_event_handler
     def create_worker(self, f):
         """
         创建worker
         f(worker)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def before_first_request(self, f):
         """
         第一个请求，请求解析为json成功后
         f(request)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def before_request(self, f):
         """
         请求解析为json成功后
         f(request)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def after_request(self, f):
         """
         执行完route对应的view_func后
         f(request, exc)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def before_response(self, f):
         """
         在 stream.write 之前，传入encode之后的data
         f(worker, response)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def after_response(self, f):
         """
         在 stream.write 之后，传入encode之后的data
@@ -106,56 +106,56 @@ class BlueprintEventsMixin(object):
     def __init__(self):
         self.events = Events()
 
-    @_register_handler
+    @_reg_event_handler
     def before_request(self, f):
         """
         请求解析为json成功后
         f(request)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def after_request(self, f):
         """
         执行完route对应的view_func后
         f(request)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def create_app_worker(self, f):
         """
         创建worker
         f(worker)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def before_app_first_request(self, f):
         """
         第一次请求，请求解析为json成功后
         f(request)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def before_app_request(self, f):
         """
         请求解析为json成功后
         f(request)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def after_app_request(self, f):
         """
         执行完route对应的view_func后
         f(request)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def before_app_response(self, f):
         """
         在 stream.write 之前，传入encode之后的data
         f(worker, response)
         """
 
-    @_register_handler
+    @_reg_event_handler
     def after_app_response(self, f):
         """
         在 stream.write 之后，传入encode之后的data
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="melon",
-    version='1.0.62',
+    version='1.0.65',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
